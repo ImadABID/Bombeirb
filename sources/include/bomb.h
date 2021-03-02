e256ccb701a978156e904ead38073b346250c7d8
ABIED Imad <Imad.Abied@bordeaux-inp.fr>
2021-03-02 16:35:18 +0100

    Merge : correction des collisions

--- sources/include/bomb.h
+++ sources/include/bomb.h
@@ -2,6 +2,6 @@
 #include <map.h>
 
 struct bomb;
-int bomb_tick(int x, int y, struct map* map);
+int bomb_tick(int x, int y);
 int bomb_place(struct player* player, struct map* map);
-int bomb_explode(int x, int y, struct map* map);
+int bomb_explode(int x, int y);
